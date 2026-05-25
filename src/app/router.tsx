0435d7da7d208e7e56931563f850667b6473860f
Nathalia Lima <95.nathallya@gmail.com>
2026-05-25 09:40:03 +0100

    change router base path

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,16 +1,11 @@
 import { createBrowserRouter } from "react-router";
-import HomePage from "@/pages/HomePage/index.tsx";
 import ProtectedMainMenu from "@/components/ProtectedMainMenu";
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/farkle-kcd-game/",
     element: <ProtectedMainMenu />,
-  },
-  {
-    path: "/login",
-    element: <HomePage />,
-  },
+  }
 ]);
 
 export default router;
